070886df34270f2a595d3ce9293140908ae75cd0
unknown <tammycuthbert427@gmail.com>
2020-07-18 10:46:37 -0500

    added wait tables

---
+++ data/waitingTable.js
@@ -0,0 +1,9 @@
+var waitTableArray =  [{
+    customerName: "Steve",
+    customerEmail: "steve32@hotmail.com", 
+    customerID: "Steve32",
+    phoneNumber: "987-654-3210",
+}
+];
+
+module.export=waitTableArray;
--- waitTableData.js
+++
@@ -1,9 +0,0 @@
-var waitTableArray =  [{
-    customerName: "Steve",
-    customerEmail: "steve32@hotmail.com", 
-    customerID: "Steve32",
-    phoneNumber: "987-654-3210",
-}
-];
-
-module.export=waitTableArray;
